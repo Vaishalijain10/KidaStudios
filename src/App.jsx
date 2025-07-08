6bad5552409675bea1f1dbd18e325183576f5bd2
Vaishalijain10 <vaishalijain.vj.10@gmail.com>
2025-07-08 15:55:34 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/KidaStudios">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
